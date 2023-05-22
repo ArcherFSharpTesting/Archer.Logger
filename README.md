0bbd514a3f6b450f40d55a2a12cc1e396fce1089
jason-kerney <jason@teamkerney.com>
2023-05-22 10:47:42 -0700

    feat: updated core types

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Archer.Logger
 A logger to be used in reporting for Archer Test Framework
+
+## Reporter Idea
+
+### Test Reporter
+
+```f#
+    // Example Test Name Reporter
+    let testNameReporter (test: ITestLocationInfo) =
+        $"%s{test.ContainerPath}.%s{test.ContainerName}.%{test.TestName}"
+        
+    let locationReporter: CodeLocation -> string = 
+        builtLocationReporter
+            {
+                TestPathReporter: testPathReporter
+                TestFileNameReporter: testFileNameReporter
+                LineNumberReporter: lineNumberReporter
+            }
+        
+    let testReporter: ITestInfo -> string = 
+        buildTestReporter
+            {
+                TestNameReporter: ITestNameInfo -> string = testNameReporeter
+                LocationReporter: CodeLocation -> string = locationReporter
+            }
+```
--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Archer.Bow
+open Archer
+open Archer.CoreTypes.InternalTypes
+open Archer.CoreTypes.InternalTypes.RunnerTypes
+open MicroLang.Lang
+
+let runner = bow.Runner ()
+
+runner.RunnerLifecycleEvent
+|> Event.add (fun args ->
+    match args with
+    | RunnerStartExecution _ ->
+        printfn ""
+    | RunnerTestLifeCycle (test, testEventLifecycle, _) ->
+        match testEventLifecycle with
+        | TestEndExecution testExecutionResult ->
+            match testExecutionResult with
+            | TestExecutionResult TestSuccess -> ()
+            | TestExecutionResult (TestFailure (TestIgnored _)) ->
+                let report = $"%A{test} : (Ignored)"
+                printfn $"%s{report}"
+            | _ ->
+                let report = $"%A{test} : (Fail) @ %i{test.Location.LineNumber}"
+                printfn $"%s{report}"
+            
+        | _ -> ()
+    | RunnerEndExecution ->
+        printfn "\n"
+)
+
+runner
+|> addMany [
+    // ?
+]
+|> runAndReport
