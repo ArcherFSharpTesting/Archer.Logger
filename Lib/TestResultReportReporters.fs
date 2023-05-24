a690c57cb06ab73418bf45d7271e172ce84e75fb
jason-kerney <jason@teamkerney.com>
2023-05-23 22:23:28 -0700

    feat: TestFailureReport Reporter

---
+++ Lib/TestResultReportReporters.fs
@@ -0,0 +1,12 @@
+﻿module Archer.Logger.TestResultReportReporters
+
+open System
+open Archer.CoreTypes.InternalTypes
+open Archer.Logger.Detail
+
+let getTestFailureReport (indentReporter: IndentReporter) (result: TestFailureReport) =
+    [
+        indentReporter.Report $"Execution Time: %A{result.Time.Total}"
+        detailedTestExecutionResultReporter indentReporter result.Test result.Result
+    ]
+    |> String.concat Environment.NewLine
---
+++ Tests/Test Result Report Reporter Should.fs
@@ -0,0 +1,46 @@
+﻿module Archer.Logger.Tests.``Test Result Report Reporter Should``
+
+open System
+open Archer
+open Archer.Arrows
+open Archer.ApprovalsSupport
+open Archer.CoreTypes.InternalTypes
+open Archer.Fletching.Types.Internal
+open Archer.Logger
+open Archer.Logger.TestResultReportReporters
+
+let private feature =
+    reporterTestBuilder
+    |> Sub.Feature (
+        TestTags [
+            Category "Summary Reporters"
+            Category "Approvals"
+        ],
+        Setup (fun reporter ->
+            let fb = TestExecutionResultFailureBuilder ()
+            Ok (reporter, fb)
+        )
+    )
+    
+let ``Format a test failure report`` =
+    feature.Test (fun (reporter, failureBuilder) environment ->
+        let testInfo = environment.TestInfo
+        let testFeature = Arrow.NewFeature ("Test", "Feature")
+        let indentReporter = IndentReporter 0
+        
+        {
+            Result = failureBuilder.GeneralExecutionFailure.CancelFailure ()
+            Time = {
+                Setup = TimeSpan (0, 0, 0, 0, 500) 
+                Test =  TimeSpan (0, 0, 0, 0, 200)
+                Teardown = TimeSpan (0, 0, 0, 10)
+                Total = TimeSpan (0, 0, 0, 0, 711)
+            }
+            Test = testFeature.Test (fun _ -> TestSuccess)
+        }
+        |> getTestFailureReport indentReporter
+        |> Should.MeetStandard reporter testInfo
+    )
+
+
+let ``Test Cases`` = feature.GetTests ()
